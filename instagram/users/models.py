28e7c14b38ecae66ccb45d4fbb68a62da9c8bef4
Jane Lee <Yesdoing13@gmail.com>
2018-11-28 00:28:18 +0900

    Creating the User Model

--- instagram/users/models.py
+++ instagram/users/models.py
@@ -1,14 +1,22 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
 
 class User(AbstractUser):
+    GENDER_CHOICES = (
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('not specified', 'Not specified')
+    )
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
-    name = CharField(_("Name of User"), blank=True, max_length=255)
-
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    website = models.URLField(null=True) 
+    bio = models.TextField(null=True)
+    phone = models.CharField(max_length=140, null=True)
+    gender = models.CharField(max_length=80, choices=GENDER_CHOICES, null=True)
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
