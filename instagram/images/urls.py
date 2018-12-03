d93e51164de7d002153ab9bcf6b706cea6189fbf
Jane Lee <Yesdoing13@gmail.com>
2018-12-03 14:27:59 +0900

    Refactoring the Like:Unlike

--- instagram/images/urls.py
+++ instagram/images/urls.py
@@ -5,7 +5,8 @@ app_name="images"
 
 urlpatterns = [
     path("", view=views.Feed.as_view(), name="feed"),
-    path("<int:image_id>/likes", view=views.LikeImage.as_view(), name="like_image"),
+    path("<int:image_id>/like", view=views.LikeImage.as_view(), name="like_image"),
+    path("<int:image_id>/unlike", view=views.UnlikeImage.as_view(), name="unlike_image"),
     path("<int:image_id>/comments", view=views.CommentOnImage.as_view(), name="comment_image"),
     path("comments/<int:comment_id>", view=views.Comment.as_view(), name="comment"),
 ]
