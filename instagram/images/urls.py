d79ef0741895164e13a5a73f8aa2edbd3a7eccc2
Jane Lee <Yesdoing13@gmail.com>
2018-12-03 13:42:51 +0900

    Deleting my own Comment

--- instagram/images/urls.py
+++ instagram/images/urls.py
@@ -5,6 +5,7 @@ app_name="images"
 
 urlpatterns = [
     path("", view=views.Feed.as_view(), name="feed"),
-    path("<int:image_id>/like", view=views.LikeImage.as_view(), name="like_image"),
-    path("<int:image_id>/comment", view=views.CommentOnImage.as_view(), name="comment_image")
+    path("<int:image_id>/likes", view=views.LikeImage.as_view(), name="like_image"),
+    path("<int:image_id>/comments", view=views.CommentOnImage.as_view(), name="comment_image"),
+    path("comments/<int:comment_id>", view=views.Comment.as_view(), name="comment"),
 ]
--- instagram/images/views.py
+++ instagram/images/views.py
@@ -85,3 +85,17 @@ class CommentOnImage(APIView):
             
             return Response(data=serializer.error, status=status.HTTP_404_NOT_FOUND)
 
+
+class Comment(APIView):
+
+    def delete(self, request, comment_id, format=None):
+
+        user = request.user
+
+        try:
+            comment = models.Comment.objects.get(id=comment_id, creator=user)
+            comment.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+            
+        except models.Comment.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
