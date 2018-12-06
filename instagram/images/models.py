691ef277c95bbc400e393c769036a6865b1d9819
Jane Lee <Yesdoing13@gmail.com>
2018-12-06 18:25:56 +0900

    Deleting an Image

--- instagram/images/models.py
+++ instagram/images/models.py
@@ -18,7 +18,7 @@ class Image(TimeStampedModel):
     file = models.ImageField()
     location = models.CharField(max_length=140)
     caption = models.TextField()
-    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True, related_name='images')
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True, related_name='images')
     tags = TaggableManager()
 
     @property
@@ -40,8 +40,8 @@ class Comment(TimeStampedModel):
     """ Comment Model """
 
     message = models.TextField()
-    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
-    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True, related_name='comments')
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True, related_name='comments')
 
     def __str__(self):
         return self.message
@@ -50,8 +50,8 @@ class Like(TimeStampedModel):
 
     """ Like Model """
 
-    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
-    image= models.ForeignKey(Image, on_delete=models.PROTECT, null=True, related_name='likes')
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+    image= models.ForeignKey(Image, on_delete=models.CASCADE, null=True, related_name='likes')
 
     def __str__(self):
         return '{} - {}'.format(self.creator.username, self.image.caption)
--- instagram/images/views.py
+++ instagram/images/views.py
@@ -37,6 +37,14 @@ class Feed(APIView):
 
 class ImageDetail(APIView):
 
+    def find_own_image(self, image_id, user):
+
+        try:
+            image = models.Image.objects.get(id=image_id, creator=user)
+            return image
+        except models.Image.DoesNotExist:
+            return None
+
     def get(self, request, image_id, format=None):
 
         user = request.user 
@@ -54,14 +62,12 @@ class ImageDetail(APIView):
 
         user = request.user
 
-        try:
-            edit_image = models.Image.objects.get(id=image_id, creator=user)
+        image = self.find_own_image(image_id, user)
 
-        except models.Image.DoesNotExist:
-
+        if image is None:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
 
-        serializer = serializers.InputImageSerializer(edit_image, data=request.data, partial=True)
+        serializer = serializers.InputImageSerializer(image, data=request.data, partial=True)
 
         if serializer.is_valid():
             
@@ -71,6 +77,19 @@ class ImageDetail(APIView):
         
         else:
             return Response(error=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, image_id, format=None):
+
+        user = request.user
+
+        image = self.find_own_image(image_id, user)
+
+        if image is None:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        image.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class LikeImage(APIView):
