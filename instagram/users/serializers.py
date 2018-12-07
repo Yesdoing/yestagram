c9736fc42d566172e0d8a19d67c20ad49aeaf8e8
Jane Lee <Yesdoing13@gmail.com>
2018-12-07 16:03:01 +0900

    Uploading profile image

--- instagram/users/serializers.py
+++ instagram/users/serializers.py
@@ -4,7 +4,7 @@ from instagram.images import serializers as image_serializers
 
 class UserProfileSerializer(serializers.ModelSerializer):
 
-    images = image_serializers.CountImageSerializer(many=True)
+    images = image_serializers.CountImageSerializer(many=True, read_only=True)
     post_count = serializers.ReadOnlyField()
     followers_count = serializers.ReadOnlyField()
     following_count = serializers.ReadOnlyField()
