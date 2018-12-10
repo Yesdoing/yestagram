472031347e388b3bead182a5810dc2ba0402169b
Jane Lee <Yesdoing13@gmail.com>
2018-12-10 17:28:32 +0900

    frontend webpack config settings && backend-frontend connected

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import style from './App.module.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className={style.App}>
+        <header className={style.appHeader}>
+          <img src={logo} className={style.appLogo} alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-            className="App-link"
+            className={style.appLink}
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
