cf4f5f3276126486d25b493f9b7de8e7ea527e96
Jane Lee <Yesdoing13@gmail.com>
2018-12-11 15:05:49 +0900

    Connecting the reducer with the React App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,19 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className={style.App}>
-        <header className={style.appHeader}>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className={style.appLink}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        
       </div>
     );
   }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore'
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+console.log(store.getState())
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById('root'));
 
