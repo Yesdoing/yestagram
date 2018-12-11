e607d39995675f08cc37c39a325f9e0655e31410
Jane Lee <Yesdoing13@gmail.com>
2018-12-11 17:50:12 +0900

    add redux middleware thunk and logger

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './redux/configureStore'
-import './index.css';
-import App from './App';
-
-console.log(store.getState())
+import store from 'redux/configureStore'
+import 'index.css';
+import App from 'App';
 
 ReactDOM.render(
     <Provider store={store}>
--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,10 +1,21 @@
-import { createStore, combineReducers} from 'redux';
-import users from './modules/user';
+import { createStore, combineReducers, applyMiddleware} from 'redux';
+import thunk from "redux-thunk";
+import users from 'redux/modules/user';
+const env = process.env.NODE_ENV;
+
+const middlewares = [thunk];
+
+
+if(env === "development") {
+    const { logger } = require('redux-logger');
+    middlewares.push(logger);
+}
+
 
 const reducer = combineReducers({
     users,
 })
 
-let store = initialState => createStore(reducer);
+let store = initialState => createStore(reducer, applyMiddleware(...middlewares));
 
 export default store();
