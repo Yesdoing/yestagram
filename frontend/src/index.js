005ac548b4b9ad6565341c4653a7ac940af99604
Jane Lee <Yesdoing13@gmail.com>
2018-12-12 16:25:34 +0900

    Syncing React Router with Redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from 'redux/configureStore'
+import store, { history } from 'redux/configureStore'
 import 'index.css';
+import { ConnectedRouter } from "connected-react-router";
+
 import App from 'App';
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <ConnectedRouter history={ history }>
+            <App />
+        </ConnectedRouter>
     </Provider>
 , document.getElementById('root'));
 
--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,21 +1,38 @@
-import { createStore, combineReducers, applyMiddleware} from 'redux';
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import users from 'redux/modules/user';
+import users from "redux/modules/user";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { connectRouter, routerMiddleware } from "connected-react-router";
+import { createBrowserHistory } from "history";
+
 const env = process.env.NODE_ENV;
 
-const middlewares = [thunk];
+export const history = createBrowserHistory();
 
+const middlewares = [thunk, routerMiddleware(history)];
 
-if(env === "development") {
-    const { logger } = require('redux-logger');
-    middlewares.push(logger);
+if (env === "development") {
+  const { logger } = require("redux-logger");
+  middlewares.push(logger);
 }
 
-
-const reducer = combineReducers({
+const reducer = history =>
+  combineReducers({
     users,
-})
+    router: connectRouter(history)
+  });
 
-let store = initialState => createStore(reducer, applyMiddleware(...middlewares));
+let store;
+
+if (env === "development") {
+  store = initialState =>
+    createStore(
+      reducer(history),
+      composeWithDevTools(applyMiddleware(...middlewares))
+    );
+} else {
+  store = initialState =>
+    createStore(reducer(history), applyMiddleware(...middlewares));
+}
 
 export default store();
