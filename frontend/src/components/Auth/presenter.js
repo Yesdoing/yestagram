02963343babaeaba5b4fddcda8aad820328799dd
Jane Lee <Yesdoing13@gmail.com>
2018-12-14 17:00:55 +0900

    Create Auth presenter component

--- frontend/src/components/Auth/presenter.js
+++ frontend/src/components/Auth/presenter.js
@@ -1,5 +1,52 @@
-import React from 'react';
-
-const Auth = (props, context) => (<h1>What's up? Bro!</h1>)
+import React from "react";
+import styles from './styles.module.scss';
+const Auth = (props, context) => (
+  <main className={styles.auth}>
+    <div className={styles.column}>
+      <img className={styles.phone} src={require('images/Phone.png')} alt="Checkout our app. Is cool "/>
+    </div>
+    <div className={styles.column}>
+      <div className={styles.whiteBox}>
+      {(() => {
+          switch(props.action) {
+            case "login":
+                return (
+                    <p>
+                        Don't have an account?{" "}
+                        <span
+                            className={styles.changeLink}
+                            onClick={props.handleChangeAction}
+                        >
+                         Sign up   
+                        </span>
+                    </p>
+                );
+            case "signup":
+                return (
+                    <p>
+                        Have an account?{" "}
+                        <span
+                            className={styles.changeLink}
+                            onClick={props.handleChangeAction}
+                        >
+                         Log in
+                        </span>
+                    </p>
+                );
+            default:
+                return null;
+          }
+      })()}
+      </div>
+      <div className={styles.appBox}>
+        <span>Get the app.</span>
+        <div className={styles.appStores}>
+          <img src={require('images/Ios.png')} alt="Download it on the Apple Appstore" />
+          <img src={require('images/Android.png')} alt="Download it on the Android Appstore" />
+        </div>
+      </div>
+    </div>
+  </main>
+);
 
 export default Auth;
