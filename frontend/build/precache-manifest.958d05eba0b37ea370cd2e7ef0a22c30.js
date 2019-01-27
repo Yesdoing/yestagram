829703aafe92ea1cca4586925532bb6fd56102e8
Jane Lee <Yesdoing13@gmail.com>
2019-01-28 01:00:38 +0900

    uncaught build

---
+++ frontend/build/precache-manifest.958d05eba0b37ea370cd2e7ef0a22c30.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a47639bc10d1a23cf5f34550ce59ced",
+    "url": "http://localhost:8000/static/media/loading.1a47639b.png"
+  },
+  {
+    "revision": "b8b300c680e172b6ad5f303f6da0eb53",
+    "url": "http://localhost:8000/static/media/Phone.b8b300c6.png"
+  },
+  {
+    "revision": "1d8fab90473146245faeb4b3ace25f01",
+    "url": "http://localhost:8000/static/media/Logo.1d8fab90.png"
+  },
+  {
+    "revision": "f06b908907d5d4f2aaf733e2bee7ea8e",
+    "url": "http://localhost:8000/static/media/Android.f06b9089.png"
+  },
+  {
+    "revision": "39cb0308e88debfdcff3",
+    "url": "http://localhost:8000/static/js/runtime~main.39cb0308.js"
+  },
+  {
+    "revision": "16d7dbc58644d2d37ed0",
+    "url": "http://localhost:8000/static/js/main.16d7dbc5.chunk.js"
+  },
+  {
+    "revision": "92debab5f7654171d407",
+    "url": "http://localhost:8000/static/js/1.92debab5.chunk.js"
+  },
+  {
+    "revision": "16d7dbc58644d2d37ed0",
+    "url": "http://localhost:8000/static/css/main.ae45e5da.chunk.css"
+  },
+  {
+    "revision": "92debab5f7654171d407",
+    "url": "http://localhost:8000/static/css/1.73b954c8.chunk.css"
+  },
+  {
+    "revision": "84de02f4a9d2b2061853891921740f65",
+    "url": "http://localhost:8000/index.html"
+  }
+];
